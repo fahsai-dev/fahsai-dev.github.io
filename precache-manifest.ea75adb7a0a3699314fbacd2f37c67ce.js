eb53687be93e0981d135ff376c976d9fc3766fd5
Fahsai <fahsai@antinode.co.th>
2020-09-04 22:10:14 +0700

    Updates

---
+++ precache-manifest.ea75adb7a0a3699314fbacd2f37c67ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d45fc17ccc9f0a122ad657064aa27ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dddb27e0678211d359eb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddb27e0678211d359eb",
+    "url": "/static/js/main.74b70a4b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
