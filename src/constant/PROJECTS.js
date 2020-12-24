a05b0e4ac6a93cff2bfaee0b02754f5a592c7475
Fahsai <fahsai@antinode.co.th>
2020-12-24 14:33:15 +0700

    Edit path image

--- src/constant/PROJECTS.js
+++ src/constant/PROJECTS.js
@@ -1,9 +1,7 @@
-const URL = process.env.PUBLIC_URL;
-
 const app = [
   {
     title: 'College Advisor',
-    thumbnail: `${URL + '/screenshot/app-advisor.png'}`,
+    thumbnail: '/screenshot/app-advisor.png',
     subtitle: 'Mobile App on iPhone Link: https://youtu.be/i0fwzywEVvc',
   },
 ]
@@ -11,32 +9,32 @@ const app = [
 const website = [
   {
     title: 'Makro Click',
-    thumbnail: `${URL + '/screenshot/web-makro.png'}`,
+    thumbnail: '/screenshot/web-makro.png',
     subtitle: 'Order Online Website',
   },
   {
     title: 'GPS Besttech',
-    thumbnail: `${URL + '/screenshot/web-besttech.png'}`,
+    thumbnail: '/screenshot/web-besttech.png',
     subtitle: 'Company Profile Website',
   },
   {
     title: 'Master Ad Dashboard',
-    thumbnail: `${URL + '/screenshot/web-dashboard3.png'}`,
+    thumbnail: '/screenshot/web-dashboard3.png',
     subtitle: 'Dashboard for overview of advertising production data',
   },
   {
     title: 'Buy Ticket',
-    thumbnail: `${URL + '/screenshot/web-buyticket3.png'}`,
+    thumbnail: '/screenshot/web-buyticket3.png',
     subtitle: 'C# Microsoft Visual Studio 2010',
   },
-  // {
-  //   title: 'Ying Ying Ying GAME',
-  //   thumbnail: `${URL + '/screenshot/web-yyygame.png'}`,
-  //   subtitle: 'GameSalad Creator',
-  // },
+  {
+    title: 'GAME Ying Ying Ying',
+    thumbnail: '/screenshot/web-yyygame4.png',
+    subtitle: 'GameSalad Creator',
+  },
   {
     title: 'Search Video',
-    thumbnail: `${URL + '/screenshot/web-findyoutube.png'}`,
+    thumbnail: '/screenshot/web-findyoutube.png',
     subtitle: 'React JS',
   },
 ]
